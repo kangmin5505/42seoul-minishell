93e6cf334d11b34cc330e3700441a257968e09b1
김상원 <gimsang-won@gimsang-won-ui-MacBookAir.local>
2022-01-25 03:51:54 +0900

    good

--- srcs2/main.c
+++ srcs2/main.c
@@ -6,20 +6,20 @@
 /*   By: gimsang-won <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 23:01:07 by gimsang-w         #+#    #+#             */
-/*   Updated: 2022/01/25 03:47:42 by gimsang-w        ###   ########.fr       */
+/*   Updated: 2022/01/25 03:51:19 by gimsang-w        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "ft_interpret.h"
 
-int	main(void)
+int	main(int n, char **args)
 {
-	char	*a = "echo >> h'l'\"l\" <<hello && hw good 'bye'\"sh\" | wow";
+	char	*a = "echo <<hello && hw good 'bye'\"sh\" | wow";
 	char	*b;
 	t_interpret	*in;
 
 	in = ft_initinlist();
-	ft_interpret(&in, a);
+	ft_interpret(&in, (n >= 2) ? args[1] : a);
 	ft_printcmds(in);
 }
