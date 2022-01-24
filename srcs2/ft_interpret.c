ab75ab8a8b91245622ae4bf66786dce5c24dfc48
김상원 <gimsang-won@gimsang-won-ui-MacBookAir.local>
2022-01-25 04:36:09 +0900

    good

--- srcs2/ft_interpret.c
+++ srcs2/ft_interpret.c
@@ -6,7 +6,7 @@
 /*   By: gimsang-won <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 19:51:27 by gimsang-w         #+#    #+#             */
-/*   Updated: 2022/01/25 04:17:02 by gimsang-w        ###   ########.fr       */
+/*   Updated: 2022/01/25 04:35:16 by gimsang-w        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,6 +88,7 @@ int	ft_redirect(t_interpret *in, char **line, int i)
 
 int	ft_next(t_interpret **in, char **line, int i)
 {
+	printf("next\n");
 	(*in)->flag = i;
 	(*in)->next = ft_initinlist();
 	*in = (*in)->next;
@@ -152,7 +153,12 @@ int ft_inerror(t_interpret *in, int flag)
 
 int	ft_parentis(t_interpret *in, char **list, int i)
 {
+	int	x;
+
 	in->son = ft_initinlist();
 	in->son->parent = in;
-	return (ft_interpret(&(in->son), *list));
+	x = ft_interpret(&(in->son), *list);
+	if (x == 0)
+		x = 2;
+	return (x);
 }
