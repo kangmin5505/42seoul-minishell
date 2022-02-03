3771d10bb6b29381d2000478711944f11482a419
김상원 <gimsang-won@gimsang-won-ui-MacBookAir.local>
2022-02-03 12:38:46 +0900

    good

--- srcs2-1/ft_pipe.c
+++ srcs2-1/ft_pipe.c
@@ -6,13 +6,13 @@
 /*   By: gimsang-won <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 05:38:38 by gimsang-w         #+#    #+#             */
-/*   Updated: 2022/02/03 11:37:13 by gimsang-w        ###   ########.fr       */
+/*   Updated: 2022/02/03 12:38:39 by gimsang-w        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_interpret.h"
 
-int	ft_process(t_interpret *in)
+int	ft_process(t_interpret *in, t_interpret **r)
 {
 	int			fd[2];
 	t_interpret	*root;
@@ -24,10 +24,11 @@ int	ft_process(t_interpret *in)
 	pid = 0;
 	ret = 1;
 	root = in;
+	tmp = 0;
 	while (root)
 	{
 		while (root->son)
-			ret = ft_process(root->son);
+			ret = ft_process(root->son, &tmp);
 		if (root->flag == PIPE)
 		{
 			pipe(fd);
@@ -40,10 +41,7 @@ int	ft_process(t_interpret *in)
 				if (root->son == 0)
 					ret &= ft_run(root);
 				else
-				{
-					tmp = ft_last(root);
-					ret &= ft_run(tmp);
-				}
+					ret &= ft_run(*tmp);
 			}
 			else
 			{
@@ -57,17 +55,17 @@ int	ft_process(t_interpret *in)
 			if (root->son == 0)
 				ret &= ft_run(root);
 			else
-			{
-				tmp = ft_last(root);
-				ret &= ft_run(tmp);
-			}
+				ret &= ft_run(*tmp);
 			if (ret != 1 && root->flag == AND)
 				return (0);
 			else if (ret == 1 && root->flag == OR)
 				return (0);
 		}
 		else if (root->parent)
+		{
+			*tmp = root;
 			return (ret);
+		}
 		else
 			ret &= ft_run(root);
 		root = root->next;
